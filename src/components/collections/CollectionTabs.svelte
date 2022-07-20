464e3499b6d74e72269d313043b5122041e7085a
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-07-20 20:23:15 +0900

    collection adding logic implementing

--- src/components/collections/CollectionTabs.svelte
+++ src/components/collections/CollectionTabs.svelte
@@ -1,11 +1,24 @@
 <script>
-	export let collections = ['Home','test'];
+    import { currentNodes } from '../../stores/nodes';
 
-    /**
+    import Icon from '@iconify/svelte';
+
+
+    if('collections' in currentNodes) {
+
+    } else {
+        // @ts-ignore
+        currentNodes['collections'] = {'Home': {}};
+    }
+
+   // @ts-ignore
+     $: collection_list = Object.keys(currentNodes['collections']);
+
+	/**
 * @param {any} ev
 */
     function renameCollectionName(ev) {
-        console.log('renameCollectionName event');
+        ev.target.disabled = false;
     }
 
     /**
@@ -19,15 +32,26 @@
 * @param {any} ev
 */
     function confirmCollectionName(ev) {
-        console.log('confirmCollectionName event');
+        if(ev.keyCode === 13) //enter
+            ev.target.disabled = true;
+    }
+
+    function addCollection() {
+        // @ts-ignore
+        currentNodes['collections']["untitled" + collection_list.length] = {};
+
     }
 </script>
 
-
 <ul>
-{#each collections as collection}
-    <li on:dblclick={renameCollectionName} on:click={focusCollection}>{collection}</li>
-    <input on:keydown={confirmCollectionName} type="text" />
+{#each collection_list as collection}
+    <li on:dblclick={renameCollectionName}>
+        <input on:keydown={confirmCollectionName}  on:click={focusCollection} type="text" bind:value={collection} disabled />
+    </li>
 {/each}
+    <li on:click={addCollection}>
+        <Icon icon="akar-icons:plus" />
+    </li>
 </ul>
+
 
