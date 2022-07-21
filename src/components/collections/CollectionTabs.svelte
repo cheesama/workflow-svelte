2f6f6da7549f4dd1ae4ad8ce1ef7a792053b00fb
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-07-22 00:42:49 +0900

    implementing collection rename logic

--- src/components/collections/CollectionTabs.svelte
+++ src/components/collections/CollectionTabs.svelte
@@ -3,6 +3,11 @@
 
 	import Icon from '@iconify/svelte';
 
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	let prevCollectionName = '';
+
 	if ('collections' in $currentNodes) {
 	} else {
 		// @ts-ignore
@@ -12,51 +17,59 @@
 	// @ts-ignore
 	$: collection_list = Object.keys($currentNodes['collections']);
 
-	// @ts-ignore
-	console.log(Object.keys($currentNodes['collections']));
-
 	/**
 	 * @param {any} ev
 	 */
 	function renameCollectionName(ev) {
 		ev.target.disabled = false;
+		prevCollectionName = ev.target.value;
 	}
 
 	/**
 	 * @param {any} ev
 	 */
 	function focusCollection(ev) {
-		console.log('focusCollection event');
+		const collectionName =
+			ev.target.tagName === 'INPUT' ? ev.target.value : ev.target.querySelector('input').value;
+
+		dispatch('changeCollection', {
+			collectionName: collectionName
+		});
 	}
 
 	/**
 	 * @param {any} ev
 	 */
 	function confirmCollectionName(ev) {
-		if (ev.keyCode === 13)
+		if (ev.keyCode === 13) {
 			//enter
 			ev.target.disabled = true;
+			dispatch('renameCollection', {
+				prevCollectionName: prevCollectionName,
+				currentCollectionName: ev.target.value
+			});
+		} else if (ev.keyCode === 27) {
+			//esc
+			ev.target.disabled = true;
+			ev.target.value = prevCollectionName;
+		}
 	}
 
 	function addCollection() {
 		// @ts-ignore
 		$currentNodes['collections']['untitled' + collection_list.length] = {};
 
-		console.log($currentNodes);
+		dispatch('addCollectionToDataFlow', {
+			collectionName: 'untitled' + collection_list.length
+		});
 	}
 </script>
 
 <ul>
 	{#each Object.keys(// @ts-ignore
 		$currentNodes['collections']) as collection}
-		<li on:dblclick={renameCollectionName}>
-			<input
-				on:keydown={confirmCollectionName}
-				on:click={focusCollection}
-				type="text"
-				bind:value={collection}
-				disabled
-			/>
+		<li on:dblclick={renameCollectionName} on:click={focusCollection}>
+			<input on:keydown={confirmCollectionName} type="text" bind:value={collection} disabled />
 		</li>
 	{/each}
 	<li on:click={addCollection}>
--- src/routes/collections/[workspace_id].svelte
+++ src/routes/collections/[workspace_id].svelte
@@ -1,5 +1,247 @@
 <script>
+	// @ts-nocheck
+
+	import { onMount } from 'svelte';
+
+	import { node_type } from '../../stores/nodes';
+	import { currentNodes, editNodeId } from '../../stores/nodes';
+
+	import ProxyNode from '../../components/nodes/ProxyNode.svelte';
+	import ProxyNodeEditModal from '../../components/nodes/ProxyNodeEditModal.svelte';
+	import RouteNode from '../../components/nodes/RouteNode.svelte';
+	import RouteNodeEditModal from '../../components/nodes/RouteNodeEditModal.svelte';
+	import ScriptNode from '../../components/nodes/ScriptNode.svelte';
+	import ScriptNodeEditModal from '../../components/nodes/ScriptNodeEditModal.svelte';
+	import SwitchNode from '../../components/nodes/SwitchNode.svelte';
+	import SwitchNodeEditModal from '../../components/nodes/ScriptNodeEditModal.svelte';
+
 	import CollectionTabs from '../../components/collections/CollectionTabs.svelte';
+
+	import Drawflow from 'drawflow';
+	import Icon from '@iconify/svelte';
+
+	let mobile_item_selec = '';
+	let mobile_last_move = null;
+	let editor;
+
+	let openProxyNodeEditModal = false;
+	let openRouteNodeEditModal = false;
+	let openScriptNodeEditModal = false;
+	let openSwitchNodeEditModal = false;
+
+	function drag(ev) {
+		if (ev.type === 'touchstart') {
+			mobile_item_selec = ev.target.closest('.drag-drawflow').getAttribute('data-node');
+		} else {
+			ev.dataTransfer.setData('node', ev.target.getAttribute('data-node'));
+		}
+	}
+
+	function allowDrop(ev) {
+		ev.preventDefault();
+	}
+
+	function drop(ev) {
+		if (ev.type === 'touchend') {
+			var parentdrawflow = document
+				.elementFromPoint(mobile_last_move.touches[0].clientX, mobile_last_move.touches[0].clientY)
+				.closest('#drawflow');
+			if (parentdrawflow != null) {
+				addNodeToDrawFlow(
+					mobile_item_selec,
+					mobile_last_move.touches[0].clientX,
+					mobile_last_move.touches[0].clientY,
+					ev
+				);
+			}
+			mobile_item_selec = '';
+		} else {
+			ev.preventDefault();
+			var nodeType = ev.dataTransfer.getData('node');
+			addNodeToDrawFlow(nodeType, ev.clientX, ev.clientY, ev);
+		}
+	}
+
+	function addNodeToDrawFlow(name, pos_x, pos_y, ev) {
+		if (editor.editor_mode === 'fixed') {
+			return false;
+		}
+		pos_x =
+			pos_x * (editor.precanvas.clientWidth / (editor.precanvas.clientWidth * editor.zoom)) -
+			editor.precanvas.getBoundingClientRect().x *
+				(editor.precanvas.clientWidth / (editor.precanvas.clientWidth * editor.zoom));
+		pos_y =
+			pos_y * (editor.precanvas.clientHeight / (editor.precanvas.clientHeight * editor.zoom)) -
+			editor.precanvas.getBoundingClientRect().y *
+				(editor.precanvas.clientHeight / (editor.precanvas.clientHeight * editor.zoom));
+
+		const node_info = $node_type[name];
+
+		const newNodeId = editor.addNode(
+			node_info.type,
+			node_info.input_num,
+			node_info.output_num,
+			pos_x,
+			pos_y,
+			node_info.type,
+			node_info.data,
+			node_info.innerHTML
+		);
+
+		let targetNode = ev.target.querySelector('#node-' + newNodeId);
+		targetNode = targetNode.querySelector('.title-box');
+		targetNode.id = newNodeId;
+
+		targetNode.addEventListener('dblclick', handleNodeDoubleClick);
+
+		updateCurrentNodes();
+	}
+
+	function positionMobile(ev) {
+		mobile_last_move = ev;
+	}
+
+	function handleNodeDoubleClick(ev) {
+		const node_type = ev.target.textContent.trim();
+
+		editNodeId.set(ev.target.id);
+		console.log($currentNodes);
+
+		switch (node_type) {
+			case 'Proxy':
+				openProxyNodeEditModal = !openProxyNodeEditModal;
+				break;
+			case 'Route':
+				openRouteNodeEditModal = !openRouteNodeEditModal;
+				break;
+			case 'Script':
+				openScriptNodeEditModal = !openScriptNodeEditModal;
+				break;
+			case 'Switch':
+				openSwitchNodeEditModal = !openSwitchNodeEditModal;
+				break;
+		}
+	}
+
+	function updateCurrentNodes() {
+		let nodesInfo = editor.export();
+		nodesInfo = { collections: nodesInfo['drawflow'] };
+
+		//backup pre-defined node information
+		if (
+			'collections' in $currentNodes &&
+			'Home' in $currentNodes['collections'] &&
+			'data' in $currentNodes['collections']['Home']
+		) {
+			for (const [nodeId, nodeData] of Object.entries(
+				$currentNodes['collections']['Home']['data']
+			)) {
+				if (nodeId in nodesInfo['collections']['Home']['data']) {
+					nodesInfo['collections']['Home']['data'][nodeId]['data'] = nodeData['data'];
+					delete nodesInfo['collections']['Home']['data'][nodeId]['html'];
+					delete nodesInfo['collections']['Home']['data'][nodeId]['typenode'];
+				}
+			}
+		}
+
+		currentNodes.set(nodesInfo);
+	}
+
+	function changeCollection(ev) {
+		editor.changeModule(ev.detail.collectionName);
+	}
+
+	function renameCollection(ev) {}
+
+	function addCollectionToDataFlow(ev) {
+		editor.addModule(ev.detail.collectionName);
+		changeCollection(ev);
+		updateCurrentNodes();
+	}
+
+	onMount(() => {
+		let id = document.getElementById('drawflow');
+		editor = new Drawflow(id);
+		editor.useuuid = true;
+		editor.start();
+
+		let elements = document.getElementsByClassName('drag-drawflow');
+		for (var i = 0; i < elements.length; i++) {
+			elements[i].addEventListener('touchend', drop, false);
+			elements[i].addEventListener('touchmove', positionMobile, false);
+			elements[i].addEventListener('touchstart', drag, false);
+		}
+
+		document.getElementById('drawflow').addEventListener('keydown', updateCurrentNodes);
+		document.getElementById('drawflow').addEventListener('mouseup', updateCurrentNodes);
+	});
 </script>
 
-<CollectionTabs />
+<div>
+	<div class="wrapper">
+		<div class="col">
+			<div class="drag-drawflow" draggable="true" on:dragstart={drag} data-node="proxy">
+				<ProxyNode />
+				<ProxyNodeEditModal
+					isModalOpen={openProxyNodeEditModal}
+					on:dblclick={() => (openProxyNodeEditModal = !openProxyNodeEditModal)}
+				/>
+			</div>
+			<div class="drag-drawflow" draggable="true" on:dragstart={drag} data-node="route">
+				<RouteNode />
+				<RouteNodeEditModal
+					isModalOpen={openRouteNodeEditModal}
+					on:dblclick={() => (openRouteNodeEditModal = !openRouteNodeEditModal)}
+				/>
+			</div>
+			<div class="drag-drawflow" draggable="true" on:dragstart={drag} data-node="script">
+				<ScriptNode />
+				<ScriptNodeEditModal
+					isModalOpen={openScriptNodeEditModal}
+					on:dblclick={() => (openScriptNodeEditModal = !openScriptNodeEditModal)}
+				/>
+			</div>
+			<div class="drag-drawflow" draggable="true" on:dragstart={drag} data-node="switch">
+				<SwitchNode />
+				<SwitchNodeEditModal
+					isModalOpen={openSwitchNodeEditModal}
+					on:dblclick={() => (openScriptNodeEditModal = !openScriptNodeEditModal)}
+				/>
+			</div>
+		</div>
+		<div class="col-right">
+			<div class="menu">
+				<CollectionTabs
+					on:addCollectionToDataFlow={addCollectionToDataFlow}
+					on:changeCollection={changeCollection}
+					on:renameCollection={renameCollection}
+				/>
+			</div>
+			<div id="drawflow" on:drop={drop} on:dragover={allowDrop}>
+				<div
+					class="btn-export"
+					on:click={Swal.fire({
+						title: 'Export',
+						html: '<pre><code>' + JSON.stringify($currentNodes, null, 4) + '</code></pre>'
+					})}
+				>
+					Export
+				</div>
+				<div class="btn-clear" on:click={editor.clearModuleSelected()}>Clear</div>
+				<div class="bar-zoom">
+					<Icon icon="akar-icons:zoom-out" on:click={editor.zoom_out()} />
+					<Icon icon="bytesize:zoom-reset" on:click={editor.zoom_reset()} />
+					<Icon icon="akar-icons:zoom-in" on:click={editor.zoom_in()} />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	#drawflow {
+		background-color: #eee;
+		width: 1400px;
+		height: 860px;
+	}
+</style>
