ea77136d08a2284f283b74a6baa745c54a21167e
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-08-01 15:07:17 +0900

    add tailwind css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,9 @@
-<h1 class="header">Workflow</h1>
+<h1 class=" text-3xl font-bold underline">Workflow</h1>
 
 <slot />
 
-<style>
-	.header {
-		width: 100%;
-		height: 40px;
-		background: #000;
-		box-sizing: border-box;
-		padding: 0px 0px 0px 20px;
-		color: #c7c7c7;
-		font-size: 20px;
-	}
+<style global lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
